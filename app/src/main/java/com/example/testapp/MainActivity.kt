f532e74a602d9d1af4d59d928b133e9c9437abed
yxspace <developer.yx@gmail.com>
2018-05-03 23:48:04 +0800

    remove gitmodule

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mylibrary.MyTestLibClass
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val asfd = MyTestLibClass()
 
 
 
